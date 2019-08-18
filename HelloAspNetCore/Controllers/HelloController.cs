c202d58d50646044d4d34988c75335a5e3e92621
Mateusz Urban <mateusz@linux.home>
2019-08-18 17:55:22 +0200

    Add instance name to hello controller

--- HelloAspNetCore/Controllers/HelloController.cs
+++ HelloAspNetCore/Controllers/HelloController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace HelloAspNetCore.Controllers
@@ -7,6 +8,7 @@ namespace HelloAspNetCore.Controllers
     [ApiController]
     public class HelloController : ControllerBase
     {
+        private static readonly string InstanceName = Guid.NewGuid().ToString();
         private readonly IOptions<MyOptions> _options;
 
         public HelloController(IOptions<MyOptions> options)
@@ -15,9 +17,13 @@ namespace HelloAspNetCore.Controllers
         }
         
         [HttpGet]
-        public ActionResult<string> Get()
+        public ActionResult<object> Get()
         {
-            return _options.Value.MyValue;
+            return new
+            {
+                InstanceName = InstanceName,
+                MyValue = _options.Value.MyValue
+            };
         }
     }
 }
