3ffe19bc941a8c597025964026522df8d1adc80f
Mateusz Urban <mateuszurban2@gmail.com>
2019-09-08 16:25:51 +0200

    Add prometheus metrics

--- HelloAspNetCore/Startup.cs
+++ HelloAspNetCore/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Prometheus;
 
 namespace HelloAspNetCore
 {
@@ -37,7 +38,9 @@ namespace HelloAspNetCore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpMetrics();
             app.UseMvc();
+            app.UseMetricServer();
         }
     }
 }
